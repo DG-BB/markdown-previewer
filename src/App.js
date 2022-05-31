fe19ce33b15521ec49c588e033044f6330628dda
Brisko Bernburg <b.bernburg@davengo.com>
2022-05-31 13:25:09 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+import {marked} from 'marked';
+
+marked.setOptions({
+    breaks: true
+});
+
+const renderer = new marked.Renderer();
+renderer.link = function (href, title, text) {
+    return `<a target="_blank" href="${href}">${text}</a>`;
+};
+
+const helperButtons = [
+    {
+        tag: "**Text**",
+        icon: "bold"
+    }, {
+        tag: "_Text_",
+        icon: "italic"
+    }, {
+        tag: "* My List Item",
+        icon: "list"
+    }, {
+        tag: "![Alternativtext](Bild-URL \"Bildtitel\")",
+        icon: "image"
+    }
+];
+
+function EditorHelperButton({tag, icon, add}) {
+
+    const handleClick = (e) => {
+        e.preventDefault();
+        add(tag);
+    }
+
+    return (
+        <button onClick={e => handleClick(e)} className="editor-helper-item">
+            <i className={"fa fa-" + icon}></i>
+        </button>
+    );
+}
+
+function Editor({val, update}) {
+    return (
+        <div className="editor-container">
+            <div style={{display: "flex"}}>
+                {helperButtons.map(value =>
+                    <EditorHelperButton key={"item-" + value.icon}  icon={value.icon} tag={value.tag}
+                                        add={() => update(val + " \n" + value.tag)}/>
+                )}
+
+            </div>
+            <textarea id="editor" value={val} onChange={event => update(event.target.value)}></textarea>
+        </div>
+    );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [text, setText] = useState('## WELCOME');
+
+    return (
+        <div className="view-container">
+
+            <Editor val={text} update={setText}/>
+
+            <div className="editor-container">
+                <div id="preview">
+                    <p style={{margin: "0"}} dangerouslySetInnerHTML={{
+                        __html: marked(text, {renderer: renderer})
+                    }}></p>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
