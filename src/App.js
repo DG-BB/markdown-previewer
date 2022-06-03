2fc8b020e1c4ad3d8d3a4db476ba050aead6b025
Brisko <b.bernburg@davengo.com>
2022-06-03 13:12:37 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ function Editor({val, update}) {
         <div className="editor-container">
             <div style={{display: "flex"}}>
                 {helperButtons.map(value =>
-                    <EditorHelperButton key={"item-" + value.icon}  icon={value.icon} tag={value.tag}
+                    <EditorHelperButton key={"item-" + value.icon} icon={value.icon} tag={value.tag}
                                         add={() => update(val + " \n" + value.tag)}/>
                 )}
 
@@ -58,7 +58,7 @@ function Editor({val, update}) {
 
 function App() {
 
-    const [text, setText] = useState('## WELCOME');
+    const [text, setText] = useState(placeholder);
 
     return (
         <div className="view-container">
@@ -66,8 +66,8 @@ function App() {
             <Editor val={text} update={setText}/>
 
             <div className="editor-container">
-                <div id="preview">
-                    <p style={{margin: "0"}} dangerouslySetInnerHTML={{
+                <div>
+                    <p id="preview" style={{margin: "0"}} dangerouslySetInnerHTML={{
                         __html: marked(text, {renderer: renderer})
                     }}></p>
                 </div>
@@ -75,5 +75,50 @@ function App() {
         </div>
     );
 }
+
+const placeholder = `# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.org), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbered lists too.
+1. Use just 1s if you want!
+1. And last but not least, let's not forget embedded images:
+
+![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+`;
 
 export default App;
